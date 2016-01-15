3ce030688699c7299f4725b71761bc1085b68cb0
GGabriele <gabrielegerbino@gmail.com>
2016-01-15 07:34:24 +0100

    Fixing apply_key_map

--- library/nxos_snmp_host.py
+++ library/nxos_snmp_host.py
@@ -211,7 +211,11 @@ def apply_key_map(key_map, table):
     for key, value in table.items():
         new_key = key_map.get(key)
         if new_key:
-            new_dict[new_key] = str(value)
+            value = table.get(key)
+            if value:
+                new_dict[new_key] = str(value)
+            else:
+                new_dict[new_key] = value
     return new_dict
 
 
--- library/nxos_switchport.py
+++ library/nxos_switchport.py
@@ -487,8 +487,13 @@ def apply_key_map(key_map, table):
     for key, value in table.items():
         new_key = key_map.get(key)
         if new_key:
-            new_dict[new_key] = str(value)
+            value = table.get(key)
+            if value:
+                new_dict[new_key] = str(value)
+            else:
+                new_dict[new_key] = value
     return new_dict
+
 
 
 def apply_value_map(value_map, resource):
